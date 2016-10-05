daf40679a8ab7bf3f204fb566fcc485bde71195c
Richard <Richard@Richard-PC.home>
2016-10-04 23:15:25 -0400

    yolo didnt test it yet

--- ChatbotPlatinum/src/groupFiles/RichardClass.java
+++ ChatbotPlatinum/src/groupFiles/RichardClass.java
@@ -11,25 +11,81 @@ public class RichardClass implements Chatbot{
 	//oldLKiving = newLivintg;
 	private String[] names = {"Joey", "Richard", "Daniel", "Victor", "Tobi", "Nockles", "Pickles", "Poon Bear", "Irene", 
 			"Amy", "Oscar", "Tina", "Jonathan", "Willy", "Dubby", "Terrance", "Jae", "Shirley", "Sara", "Taylor"};
-	private String[] wordsHurt = {"You suck at this", "What a n00b, learn to think", "Give up", "Uninstall please"};
-	private boolean inKillLoop;
+	private String[] wordsHurt = {"You suck at this", "What a n00b, learn to think", "Give up", "Uninstall please","LOSER, KEEP THAT 'L'"};
+	private String[] encouragement = {"Are you even trying??", "Oops, I cheated", "Just type 'QUIT' alreay, we all know you're a failure",
+			"What's that in your head? Oh, nevermind its just an empty shell", "Are you done or are you dead?", "Take the 'L' man, stop wasting everyone's time"};
+	static boolean inKillLoop;
 	private int killCount;
 	private int totalHeadCount;
-	
+	static String response;
+	public String[] people;
+	public String[] mafia;
+	private String beep; 
 	public RichardClass(){
 		killCount = 0;
 		totalHeadCount = 12;
 
 		generateLiving();
+		killLoop();
 		}
 	private void generateLiving(){
-//		Player[] living;
 		String[] players;
 		players = new String[totalHeadCount];
 		System.arraycopy(pickVictums(), 0, players, 0, totalHeadCount);
-		setMafia(players);
-		System.out.println(randomize(players));
-		
+		mafia = setMafia(players);
+		people = randomize(players);
+		System.out.println(people);
+		killLoop();
+	}
+	public void killLoop(){
+		inKillLoop = true;
+		while(inKillLoop == true){
+			DanielMain.print("Who do you want to kill?");
+			response = DanielMain.promptInput();
+			if(checkResponse(response) == false){
+				DanielMain.print("Dude are you stupid? Does that look like a listed name?");
+			}
+			if(checkResponse(response) == true){
+				replace(people, response, "");
+				killCount++;
+				mafiaKill();
+				int e = (int) ((double)Math.random()*encouragement.length)-1;
+				DanielMain.print(encouragement[e]);
+			}
+			if(people.length == 4){
+				inKillLoop = false;
+				loser();
+				
+			}
+			if(mafia[0] == null && mafia[1] == null){
+				inKillLoop = false;
+				win();
+			}
+		}
+	}
+	private void loser(){
+		int l = (int) ((double)Math.random()*wordsHurt.length)-1;
+		DanielMain.print(wordsHurt[l]);
+	}
+	private void win(){
+		DanielMain.print("Cheater, I hope you die");
+	}
+	private void mafiaKill(){
+		int deathByMaf = (int) ((double) Math.random()*(people.length - killCount));
+		if(people[deathByMaf] == mafia[0] || people[deathByMaf] == mafia[1] || people[deathByMaf] == ""){
+			mafiaKill();
+		}
+		else{
+			replace(people, people[deathByMaf], "");
+		}
+	}
+	private boolean checkResponse(String response){
+		for(int i = 0; i <= totalHeadCount-1; i++){
+			if(response == people[i]){
+				return true;
+			}
+		}	
+		return false;
 	}
 
 	public String[] pickVictums(){	
@@ -59,7 +115,7 @@ public class RichardClass implements Chatbot{
 			}
 		}
 	}
-	public void setMafia(String[]list){
+	public String[] setMafia(String[]list){
 		String[]mafia;
 		mafia = new String[2];
 	//	if((list.length % 2) == 0){
@@ -72,7 +128,7 @@ public class RichardClass implements Chatbot{
 	//	else{		
 	//	}
 			}
-			return;
+			return mafia;
 	}
 	public String[] randomize(String[]players){
 		String[]randomized;
@@ -98,7 +154,16 @@ public class RichardClass implements Chatbot{
 		
 	@Override
 	public void talk() {
-		
+		inRichardClassLoop = true;
+		//static call on promptInput method from RichardMain class
+		while(inRichardClassLoop){
+			DanielMain.print("(Type 'quit' to go back)");
+			beep = DanielMain.promptInput();
+			if(beep.indexOf("quit") >= 0){
+				inRichardClassLoop = false;
+				DanielMain.promptForever();
+			}
+		}
 	}
 
 	@Override
